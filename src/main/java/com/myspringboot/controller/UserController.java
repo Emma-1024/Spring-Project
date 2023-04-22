477a0de824c8ec92987aec97b6851ecf4a293ba9
Emma <yongjie.wu@outlook.com>
2023-04-22 20:24:05 +0800

    refactor: rename controller

--- src/main/java/com/myspringboot/controller/UserController.java
+++ src/main/java/com/myspringboot/controller/UserController.java
@@ -1,45 +1,29 @@
 package com.myspringboot.controller;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
-import com.myspringboot.dto.Greeting;
 import com.myspringboot.model.User;
-import com.myspringboot.mapper.UserMapper;
 import com.myspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class MyController {
+import java.util.List;
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+@RestController
+public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
-    @GetMapping("/createUser")
-    public User createUser() {
-        // insert one user data
-        User user = new User();
-        user.setName("Emily");
-        user.setEmail("emily@gmail.com");
-        user.setPassword("2222");
-        user.setPhone_number("13456783456");
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
         userService.insert(user);
         return user;
     }
 
+
     @GetMapping("/user")
-    public User getUserById(@RequestParam(value = "id", required = false) Long id, @RequestParam(value = "name", required = false) String name) {
+    public User getUser(@RequestParam(value = "id", required = false) Long id, @RequestParam(value = "name", required = false) String name) {
         if (id != null) {
             return userService.getUserById(id);
         } else if (name != null) {
@@ -59,6 +43,4 @@ public class MyController {
         userService.update(id, "bob", "test090909");
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
-

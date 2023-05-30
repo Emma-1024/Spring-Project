cd7dd7918f0b3cb1f5debb27b1e07f09b89171b4
Emma <yongjie.wu@outlook.com>
2023-05-30 21:47:21 +0800

    feat: add java JSONObject
    Add a GetMapping splitStr for regex test

--- src/main/java/com/myspringboot/controller/GreetingController.java
+++ src/main/java/com/myspringboot/controller/GreetingController.java
@@ -4,9 +4,17 @@
  */
 package com.myspringboot.controller;
 
+import com.diffplug.spotless.maven.json.Json;
 import com.myspringboot.vo.Greeting;
+
+import java.util.Collections;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import org.json.JSONObject;
+
 
 @RestController
 public class GreetingController {
@@ -14,8 +22,26 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.debug("Hello from Log4j 1");
+        logger.warn("Hello from Log4j 2");
+        logger.error("Hello from Log4j 3");
+        logger.info("{}", 5);
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/splitStr")
+    public String splitStr() {
+        String input = "This is a string with, some, commas,but not, in, quote s, \"Like this, haha, hi!\", and another, \"Like this, one, too!\"";
+        String regex = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+        String[] items = input.split(regex);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("string", input);
+        jsonObject.put("regex", regex);
+        jsonObject.put("result", items);
+        return jsonObject.toString();
     }
 }
